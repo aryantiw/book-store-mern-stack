41b6909e11c40cdc1156ba1473455062e0db82d6
aryantiw <aryantiwari543@gmail.com>
2023-09-26 19:02:29 +0530

    inint

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,5 @@
 export const PORT = 5555;
 
+export const mongoDBURL = 'mongodb+srv://root:root@book-store-mern.mdqoghy.mongodb.net/books-collection?retryWrites=true&w=majority'
+
+//external things needed to hide
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,50 @@
-import express from "express";
-import { PORT } from "./config.js";
+import express, { request, response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import { router } from './routes/bookksRoutes.js';
+import  cors from 'cors';
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`)
+
+app.use(express.json());
+
+
+//option 1: allow all origins with default of cors(*)
+//app.use(cors());
+
+//option2: allow custom origins
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'PUT', 'POST', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
+
+
+//middleware for parsing request body
+app.use('/books', router);
+
+
+app.get('/', (request, response)=>{
+    console.log(request)
+    return response.status(234).send('i dont know what this is')
+});
+
+//connecting mongoose server
+mongoose
+.connect(mongoDBURL)
+.then(()=>{
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+        console.log(`App is listening to port: ${PORT}`)
+    });
+    
+})
+.catch((error) =>{
+    console.log(error);
 });
