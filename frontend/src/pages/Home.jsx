f26ce30e2babee61b1acb01845e0fe104dd9ff44
aryantiw <aryantiwari543@gmail.com>
2023-10-06 18:43:15 +0530

    components

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import Spinner from '../components/spinner';
+import { Link } from 'react-router-dom';
+import {AiOulineEdit} from 'react-icons/ai'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
