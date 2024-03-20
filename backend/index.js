66b268205b545339b04908483543305de0283ae5
aryantiw <aryantiwari543@gmail.com>
2024-03-20 11:08:14 +0530

    fin com

--- backend/index.js
+++ backend/index.js
@@ -16,13 +16,13 @@ app.use(express.json());
 
 //option2: allow custom origins
 
-// app.use(
-//     cors({
-//         origin: 'http://localhost:3000',
-//         methods: ['GET', 'PUT', 'POST', 'DELETE'],
-//         allowedHeaders: ['Content-Type'],
-//     })
-// );
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'PUT', 'POST', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
 
 
 
